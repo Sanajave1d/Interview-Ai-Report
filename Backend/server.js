f35bf71f88ae2c3459bd7c3d3d2c1ae43c38b376
Sanajav1d <khawerkhan295@gmail.com>
2026-08-26 18:35:33 +0500

    added railway json file

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,8 @@ const { connectToDB } = require("./src/config/db");
 connectToDB()
 
 
-app.listen(3000,()=>{
-    console.log("app running at port 3000")
+const port = process.env.PORT || 3000
+
+app.listen(port,()=>{
+    console.log(`app running at port ${port}`)
 })
